4d23483d54c02f127fe51360f8bd67613b3bb2da
alexanderholman <alexander@holman.org.uk>
2016-04-05 02:10:37 +0100

    add 301 redirect file

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * This file is here for the purpose of redirecting from
+ * http://text-file-stats.herokuapp.com to http://text-file-stats.herokuapp.com/example
+ */
+
+header( "Location: /example", true, 301 );
